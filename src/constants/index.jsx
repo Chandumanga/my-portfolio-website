6fa04c0854984aa9aa1ce97aa9affd9ea3edb954
Chandu <chandumanga702@gmail.com>
2025-01-01 10:01:28 +0530

    Added Ttile and updated the github link in footer

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -267,7 +267,7 @@ export const SOCIAL_MEDIA_LINKS = [
     icon: <FaXTwitter fontSize={25} className="hover:opacity-80" />,
   },
   {
-    href: "https://github.com/",
+    href: "https://github.com/Chandumanga/",
     icon: <FaGithub fontSize={25} className="hover:opacity-80" />,
   },
   {
